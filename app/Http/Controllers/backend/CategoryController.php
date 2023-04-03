1bc78f2dea760638759d754131eb374404238d16
thanhtrung48 <91000592+thanhtrung48@users.noreply.github.com>
2023-04-03 08:18:54 +0700

    Add files via upload

--- app/Http/Controllers/backend/CategoryController.php
+++ app/Http/Controllers/backend/CategoryController.php
@@ -4,15 +4,15 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\category;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $list_category = Category::where('status','!=',0)->get();
+        return view('backend.category.index',compact('list_category'));
     }
 
     /**
@@ -22,6 +22,7 @@ class CategoryController extends Controller
     {
         //
     }
+    
 
     /**
      * Store a newly created resource in storage.
@@ -36,7 +37,13 @@ class CategoryController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $category = Category::find($id);
+        if ($category == null) {
+            return redirect()->route('category.index')->with('message',['type'=>'danger','msg' 
+            =>'Mẫu tin không tồn tại!']);
+        }else{
+            return view('backend.category.show',compact('category'));
+        }
     }
 
     /**
@@ -58,8 +65,41 @@ class CategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      */
+
     public function destroy(string $id)
     {
         //
     }
+    //GET:admin/category/status/1
+    public function status($id)
+    {
+        $category = Category::find($id);
+         if ($category == null) {
+            return redirect()->route('category.index')->with('message',['type'=>'danger','msg' 
+            =>'Mẫu tin không tồn tại!']);
+        }else{
+            $category->status = ($category->status ==  1) ? 2 : 1;
+            $category->updated_by = date('y-m-d H:i:s');
+            $category->updated_by = 1;
+            $category->save();
+            return redirect()->route('category.index')->with('message',['type'=>'success','msg' 
+            =>'Thay đổi trang thái thành công!']);
+        }
+     }
+       //GET:admin/category/delete/1
+    public function delete($id)
+    {
+        $category = Category::find($id);
+         if ($category == null) {
+            return redirect()->route('category.index')->with('message',['type'=>'danger','msg' 
+            =>'Mẫu tin không tồn tại!']);
+        }else{
+            $category->status = 0;
+            $category->updated_by = date('y-m-d H:i:s');
+            $category->updated_by = 1;
+            $category->save();
+            return redirect()->route('category.index')->with('message',['type'=>'success','msg' 
+            =>'Xóa vào thùng rác thành công!']);
+        }
+     }
 }
